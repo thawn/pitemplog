acdfae74c320f662c972ce9a2685da74b8cf7854
Till Korten <webmaster@korten.at>
2019-05-01 16:11:50 +0200

    modified install.sh to work in a docker container

--- _src/_bin/install.sh
+++ _src/_bin/install.sh
@@ -1,30 +1,56 @@
 #!/bin/bash
-target_dir=/usr/local/share/templog/
-sudo mkdir -p "$target_dir"
-sudo chown pi:pi "$target_dir"
-sudo chown pi:pi /var/www
-sudo chmod a+rwx /var/www
+if [ "x$INSTALL_DIR" == "x" ]; then
+  target_dir=/usr/local/share/templog/
+else
+  target_dir=$INSTALL_DIR
+fi
+if [ "x$INSTALL_DIR" == "x" ]; then
+  target_dir=/usr/local/share/templog/
+else
+  target_dir=$INSTALL_DIR
+fi
+echo "Installing into: $INSTALL_DIR"
+echo "Local sensors: $LOCAL_SENSORS"
+mkdir -p "$target_dir"
+chown pi:pi "$target_dir"
+chown pi:pi /var/www
+chmod a+rwx /var/www
 chmod a+x "${target_dir}"_bin/*.{sh,py}
 chmod u+x "${target_dir}"_sbin/*.sh
 chmod a+w "${target_dir}"
 chmod a+w "${target_dir}"conf/config.json
 chmod a+w "${target_dir}"_data/config.json
 chmod a+x "${target_dir}"_data/*.py
-sudo cp "${target_dir}"_sbin/templog.conf /etc/apache2/sites-available/
-sudo chown root:root /etc/apache2/sites-available/templog.conf
-sudo rm -f /etc/apache2/sites-enabled/0000-templog.conf
-sudo ln -s /etc/apache2/sites-available/templog.conf /etc/apache2/sites-enabled/0000-templog.conf
-sudo service apache2 restart
+cp "${target_dir}"_sbin/templog.conf /etc/apache2/sites-available/
+chown root:root /etc/apache2/sites-available/templog.conf
+rm -f /etc/apache2/sites-enabled/0000-templog.conf
+ln -s /etc/apache2/sites-available/templog.conf /etc/apache2/sites-enabled/0000-templog.conf
 if ! [ -e /usr/local/bin/jekyll ]; then
-  sudo ln -s /usr/bin/jekyll /usr/local/bin/jekyll
+  ln -s /usr/bin/jekyll /usr/local/bin/jekyll
 fi
-sudo -u www-data "${target_dir}"_data/create_pages.py
-sudo -u www-data /usr/local/bin/jekyll build --source "${target_dir}"
-sudo cp "${target_dir}"_bin/*.py /usr/local/bin/
-sudo cp "${target_dir}"_bin/pitemplog_backup.sh /usr/local/bin/
-sudo cp "${target_dir}"_bin/pitemplog_restore.sh /usr/local/bin/
-sudo cp "${target_dir}"_bin/pitemplog.conf /etc/
-sudo cp "${target_dir}"_sbin/pitemplog_partition_database.sh /usr/local/sbin/
-sudo crontab "${target_dir}"_sbin/crontab_root
-sudo -u pi crontab "${target_dir}"_bin/crontab
+su - www-data -s /bin/bash -c "/usr/bin/python \"${target_dir}\"_data/create_pages.py"
+su - www-data -s /bin/bash -c "/usr/local/bin/jekyll build --source \"${target_dir}\""
+cp "${target_dir}"_bin/*.py /usr/local/bin/
+cp "${target_dir}"_bin/pitemplog_backup.sh /usr/local/bin/
+cp "${target_dir}"_bin/pitemplog_restore.sh /usr/local/bin/
+cp "${target_dir}"_bin/pitemplog.conf /etc/
+cp "${target_dir}"_sbin/pitemplog_partition_database.sh /usr/local/sbin/
+crontab "${target_dir}"_sbin/crontab_root
+cgroup=$(grep cpuset /proc/1/cgroup | cut -d ':' -f 3)
+if [ "$LOCAL_SENSORS" == "no" ]; then
+  crontab -u pi "${target_dir}"_bin/crontab_nosensors
+else
+  crontab -u pi "${target_dir}"_bin/crontab
+fi
+echo "Installation successful. (re)starting apache now."
+if [ "${cgroup#/docker}" == "$cgroup" ]; then
+  #if we are not in a docker container, we restart the apache service
+  service apache2 restart
+else
+  #In a docker container, apache runs as foreground process and is passed any arguments passed to the container
+  if [ "${1#-}" != "$1" ]; then
+	set -- apache2-foreground "$@"
+  fi
+  exec "$@"
+fi
 
